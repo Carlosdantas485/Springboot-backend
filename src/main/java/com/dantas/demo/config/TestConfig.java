23515a29ac2de608624b5a49e58ed28155bac08d
Carlos Alberto Dantas Filho <carlosdantas485@gmail.com>
2022-06-25 15:37:16 -0300

    Order status enum

--- src/main/java/com/dantas/demo/config/TestConfig.java
+++ src/main/java/com/dantas/demo/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.dantas.demo.entities.Order;
 import com.dantas.demo.entities.User;
+import com.dantas.demo.entities.enums.OrderStatus;
 import com.dantas.demo.repositories.OrderRepository;
 import com.dantas.demo.repositories.UserRepository;
 
@@ -33,9 +34,9 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria do Carmo", "maria@123", "987654321", "senha", "1000");
 		User u2 = new User(null, "Carlos Dantas", "carlos@123", "987654321", "senha", "5000");
 		
-		Order o1 = new Order(null, Instant.parse("2022-06-20T19:53:07z"), u1);
-		Order o2 = new Order(null, Instant.parse("2022-07-21T03:42:10z"), u2);
-		Order o3 = new Order(null, Instant.parse("2022-07-22T15:21:22z"), u1);
+		Order o1 = new Order(null, Instant.parse("2022-06-20T19:53:07z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2022-07-21T03:42:10z"), OrderStatus.WAITING_PAYMANT, u2);
+		Order o3 = new Order(null, Instant.parse("2022-07-22T15:21:22z"), OrderStatus.WAITING_PAYMANT, u1);
 		
 		//salva os usuarios no banco de dados
 		//Pasando uma lista de usuarios 
--- src/main/java/com/dantas/demo/entities/Order.java
+++ src/main/java/com/dantas/demo/entities/Order.java
@@ -12,9 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.dantas.demo.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonValueFormat;
 
 @Entity
 @Table(name = "tb_Orders")
@@ -27,6 +26,8 @@ public class Order implements Serializable {
 
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'z'", timezone = "GMT")
 	private Instant moment;
+	
+	private Integer orderStatus;
 
 	// para falar que um pedito tem um usuario
 	@ManyToOne
@@ -36,10 +37,11 @@ public class Order implements Serializable {
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -57,6 +59,16 @@ public class Order implements Serializable {
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus) ;
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	public User getClient() {
