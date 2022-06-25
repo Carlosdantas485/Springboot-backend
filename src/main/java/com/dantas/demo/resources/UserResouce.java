a771a0f302f29136559905beac75f81ac70e605a
Carlos Alberto Dantas Filho <carlosdantas485@gmail.com>
2022-06-25 12:13:54 -0300

    Service layer, component registation

--- src/main/java/com/dantas/demo/resources/UserResouce.java
+++ src/main/java/com/dantas/demo/resources/UserResouce.java
@@ -1,19 +1,38 @@
 package com.dantas.demo.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dantas.demo.entities.User;
+import com.dantas.demo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResouce {
 	
+	//dependencia para o servce 
+	@Autowired
+	private UserService service;
+	
+	//Ira retornar a lista de usuarios
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@123", "987654321","123456789", "0,0");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
+	
+	//Ira retornar a lista de usuarios por ID
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	
 }
---
+++ src/main/java/com/dantas/demo/services/UserService.java
@@ -0,0 +1,34 @@
+package com.dantas.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dantas.demo.entities.User;
+import com.dantas.demo.repositories.UserRepository;
+
+
+// Registra a classe como um componente de servico do spring 
+@Service
+public class UserService  {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	// Metodo para retornar todos os usuarios do banco de dados
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional <User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	
+	
+
+}
