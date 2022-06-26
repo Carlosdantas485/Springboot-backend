28f98e8dd35aca9bdf1456588a82dba1a87e77bf
Carlos Alberto Dantas Filho <carlosdantas485@gmail.com>
2022-06-26 14:11:33 -0300

    Update order status

--- src/main/java/com/dantas/demo/resources/OrderResouce.java
+++ src/main/java/com/dantas/demo/resources/OrderResouce.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +33,13 @@ public class OrderResouce {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Order> findById(@PathVariable Long id) {
 		Order obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PutMapping(value = "/{id}")
+	//os parametros do metodo sao o Id do usuario e o conteudo a ser enviado para a API
+	public ResponseEntity<Order> update(@PathVariable Long id, @RequestBody Order obj){
+		obj = service.update(id, obj);
 		return ResponseEntity.ok().body(obj);
 	}
 
--- src/main/java/com/dantas/demo/services/OrderService.java
+++ src/main/java/com/dantas/demo/services/OrderService.java
@@ -9,26 +9,33 @@ import org.springframework.stereotype.Service;
 import com.dantas.demo.entities.Order;
 import com.dantas.demo.repositories.OrderRepository;
 
-
 // Registra a classe como um componente de servico do spring 
 @Service
-public class OrderService  {
-	
+public class OrderService {
+
 	@Autowired
 	private OrderRepository repository;
-	
+
 	// Metodo para retornar todos os usuarios do banco de dados
-	
-	public List<Order> findAll(){
+
+	public List<Order> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public Order findById(Long id) {
-		Optional <Order> obj = repository.findById(id);
+		Optional<Order> obj = repository.findById(id);
 		return obj.get();
 	}
-	
-	
-	
+
+	public Order update(Long id, Order obj) {
+		Order entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(Order entity, Order obj) {
+		entity.setOrderStatus(obj.getOrderStatus());
+		
+	}
 
 }
